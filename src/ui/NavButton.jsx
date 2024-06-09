e80c042d401e6bc284b267b663889ec3167876ab
altomizawa <alyssontomizawa@hotmail.com>
2024-06-09 14:13:30 -0300

    style: add media query for navmenu

--- src/ui/NavButton.jsx
+++ src/ui/NavButton.jsx
@@ -3,7 +3,7 @@ import './NavButton.css'
 function NavButton({ isMenuOpen, setIsMenuOpen }) {
   
   return (
-    <div className='navbutton' onClick={() => {setIsMenuOpen(prev=>!prev)}}>
+    <div className={isMenuOpen ? 'navbutton navbutton_active' : 'navbutton'} onClick={() => {setIsMenuOpen(prev=>!prev)}}>
       <div className={isMenuOpen ? 'navbutton__menu-line navbutton__menu-line_top' : 'navbutton__menu-line'}></div>
       <div className={isMenuOpen ? 'navbutton__menu-line navbutton__menu-line_middle' : 'navbutton__menu-line'}></div>
       <div className={isMenuOpen ? 'navbutton__menu-line navbutton__menu-line_bottom' : 'navbutton__menu-line'}></div>
