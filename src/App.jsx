10569f1f9acef4fa796b61e6275e7d13d2c1e4c0
Al Tomizawa <alyssontomizawa@hotmail.com>
2024-06-10 12:11:51 -0300

    feat: change title in index.html and add a dynamic route for work

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/work' element={<Work />}/>
+        <Route path='/work/:client' element={<Contact/>} />
         <Route path='/contact' element={<Contact />}/>
       </Routes>
   )
