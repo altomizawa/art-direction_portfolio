c65122e53679d9c915545437fc872f7777703fce
altomizawa <alyssontomizawa@hotmail.com>
2024-06-10 17:06:15 -0300

    add work images

---
+++ src/database/db.js
@@ -0,0 +1,42 @@
+export const db = [
+  {
+    client: "JBL",
+    campaign: "Destination Anywhere Tour",
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis fermentum ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse metus nibh, pellentesque finibus dolor a, molestie vulputate ante. Ut tempus nibh nulla, quis facilisis urna condimentum non. Integer euismod id lacus et eleifend. In vitae libero consectetur, luctus eros vel, interdum ante. Aenean quis pretium lorem. Duis tempor nunc sit amet eros interdum, iaculis luctus turpis ullamcorper. Donec vitae condimentum orci, in interdum orci. Integer venenatis tortor at sem commodo faucibus.',
+    imageLinks: [
+    {
+      name: 'subway poster',
+      link: '',
+    },
+    {
+      name: 'subway poster',
+      link: '',
+    },
+    {
+      name: 'subway poster',
+      link: '',
+    },
+    {
+      name: 'subway poster',
+      link: '',
+    },
+    {
+      name: 'subway poster',
+      link: '',
+    },
+    {
+      name: 'subway poster',
+      link: '',
+    },
+
+  ],
+  hasVideo: false,
+  videoLinks: [
+    {
+      name: '',
+      link: '',
+    }
+  ]
+
+  }
+]
