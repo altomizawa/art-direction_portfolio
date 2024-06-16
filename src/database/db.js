1bc74a7e99a9120d0834001bbd043db8029a0319
altomizawa <alyssontomizawa@hotmail.com>
2024-06-16 12:47:44 -0300

    feat: add more description in db

--- src/database/db.js
+++ src/database/db.js
@@ -260,7 +260,7 @@ export const db = [
     id: 7,
     name: "Playstation Socom",
     campaign: "Orchestrate the perfect op.",
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis fermentum ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse metus nibh, pellentesque finibus dolor a, molestie vulputate ante. Ut tempus nibh nulla, quis facilisis urna condimentum non. Integer euismod id lacus et eleifend. In vitae libero consectetur, luctus eros vel, interdum ante. Aenean quis pretium lorem. Duis tempor nunc sit amet eros interdum, iaculis luctus turpis ullamcorper. Donec vitae condimentum orci, in interdum orci. Integer venenatis tortor at sem commodo faucibus.',
+    description: "We had to create an ad for the upcoming game SOCOM Navy Seals - Tactical Strike. This game faithfully replicates NAVY Seals' strategies and tactics. We came up with an idea that mirrors a real-life scenario to showcase the Seals' precision.",
     hasPrint: true,
     imageLinks: [
     {
@@ -276,7 +276,7 @@ export const db = [
     id: 8,
     name: "Uncle Ben's",
     campaign: "Bring more to the table.",
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis fermentum ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse metus nibh, pellentesque finibus dolor a, molestie vulputate ante. Ut tempus nibh nulla, quis facilisis urna condimentum non. Integer euismod id lacus et eleifend. In vitae libero consectetur, luctus eros vel, interdum ante. Aenean quis pretium lorem. Duis tempor nunc sit amet eros interdum, iaculis luctus turpis ullamcorper. Donec vitae condimentum orci, in interdum orci. Integer venenatis tortor at sem commodo faucibus.',
+    description: "How satisfying can a meal be if prepared in just 90 seconds? To promote Uncle Ben's new product, the 90-second rice, we devised a fun concept that demonstrates you can enjoy a truly special dish, even when it's made in the microwave.",
     hasPrint: false,
     imageLinks: [],
     hasVideo: true,
