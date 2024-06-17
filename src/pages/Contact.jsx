441b03d52e5d2864a328535e760a19c86020b4a3
altomizawa <alyssontomizawa@hotmail.com>
2024-06-17 13:18:45 -0300

    feat: add toast when user clicks on email

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,13 @@
 import arrow from '../assets/arrow_forward_24dp_FILL0_wght400_GRAD0_opsz24.svg'
 import './Contact.css'
+import toast, { Toaster } from 'react-hot-toast';
 
 function Contact() {
+  function copyToClipboard () {
+    navigator.clipboard.writeText('alyssontomizawa@hotmail.com');
+    toast('e-mail address copied to clipboard');
+  } 
+
   return (
     <div className='contact'>
       <div className='contact__container'>
@@ -10,11 +16,12 @@ function Contact() {
           <p>(choose one way to connect)</p>
         </div>
         <div className='contact__links'>
-          <a href='/' target='_blank'>EMAIL<img src={arrow} /></a>
+          <button onClick={copyToClipboard}>EMAIL<img src={arrow} /></button>
           <a href='https://www.linkedin.com/in/alysson-tomizawa-672a586/' target='_blank'>LINKEDIN<img src={arrow} /></a>
           <a href='https://github.com/altomizawa' target='_blank'>GITHUB<img src={arrow} /></a>
           <a href='https://wa.me/5561998133337' target='_blank'>WHATSAPP<img src={arrow} /></a>
         </div>
+        <Toaster />
       </div>
     </div>
   )
