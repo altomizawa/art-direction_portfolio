81a618df2c4cdc2e0572e08e3327dee656ade00c
altomizawa <alyssontomizawa@hotmail.com>
2024-06-17 12:48:50 -0300

    feat: add contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,23 @@
+import arrow from '../assets/arrow_forward_24dp_FILL0_wght400_GRAD0_opsz24.svg'
+import './Contact.css'
 
 function Contact() {
   return (
-    <div>
-      Contact page
+    <div className='contact'>
+      <div className='contact__container'>
+        <div className='contact__title'>
+          <h1>CONTACT ME</h1>
+          <p>(choose one way to connect)</p>
+        </div>
+        <div className='contact__links'>
+          <a href='/'>EMAIL<img src={arrow} /></a>
+          <a href='/' >LINKEDIN<img src={arrow} /></a>
+          <a href='/' >GITHUB<img src={arrow} /></a>
+          <a href='/' >WHATSAPP<img src={arrow} /></a>
+        </div>
+      </div>
     </div>
   )
-}
+} 
 
 export default Contact
